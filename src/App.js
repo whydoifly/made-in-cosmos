2bceb1fc9938ea12dbb3e4506a93cefe66fac517
whydoifly <netpreecher@gmail.com>
2022-07-18 23:08:27 +0300

    Swiper added
    - Added swiper instance
    
    - Removed useless files
    
     - Tried to add main brand word with correct font style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from './header';
-
+import MainScreen from './mainScreen';
 
 function App() {
   return (
     <div className="App basic-container">
       <Header />
+      <MainScreen />
     </div>
   );
 }
---
+++ src/mainScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainSwiper from './mainSwiper';
+
+const MainScreen = () => {
+    return (
+        <>
+            <div className="big-brand-word">MADE IN COSMOS</div>
+            <MainSwiper />
+        </>
+    );
+}
+
+export default MainScreen;
